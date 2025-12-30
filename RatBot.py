2d01c9c022578bfa4c908ed24fe97562ea7859d8
Anurag Tomar <anuragtomar259@gmail.com>
2025-12-30 18:41:44 +0530

    Refactor get_best_move and evaluate_board methods

--- RatBot.py
+++ RatBot.py
@@ -1,6 +1,7 @@
 import sys
 import copy
 import time
+import random
 from config import *
 from board import Board
 
@@ -8,7 +9,7 @@ class RatBot:
     def __init__(self, board):
         self.board = board
         self.nodes_expanded = 0
-        self.depth = 1 ## set depth as you see fit and use it further for your works. 
+        self.depth = 1
 
     def get_best_move(self):
         legal_moves = self.board.get_legal_moves()
@@ -22,20 +23,20 @@ class RatBot:
         for move in legal_moves:
             self.board.make_move(move)
 
-        score = self.evaluate_board()
-        
-        self.board.undo_move()
+            score = self.evaluate_board()
 
-        if score > best_score:
-            best_score = score
-            best_moves = [move]
-        elif score == best_score:
-            best_moves.append(move)
+            self.board.undo_move()
+
+            if score > best_score:
+                best_score = score
+                best_moves = [move]
+            elif score == best_score:
+                best_moves.append(move)
 
         return random.choice(best_moves)
 
     def evaluate_board(self):
-        piece_values = { 
+        piece_values = {
             'P': 20,
             'N': 70,
             'B': 70,
@@ -44,21 +45,22 @@ class RatBot:
 
         score = 0
 
-for row in self.board.board:
-    for piece in row:
-        if piece == EMPTY_SQUARE:
-            continue
+        for row in self.board.board:
+            for piece in row:
+                if piece == EMPTY_SQUARE:
+                    continue
 
-        value = piece_values[piece[1]]
+                value = piece_values[piece[1]]
 
-if piece[0] == 'w':
-    score += value
-else:
-    score -= value
+                if piece[0] == 'w':
+                    score += value
+                else:
+                    score -= value
 
-if self.board.is_in_check():
-    score -= 2 if self.board.white_to_move else 2
+        if self.board.is_in_check():
+            score -= 2 if self.board.white_to_move else 2
 
-return score
+        return score
+
         
 
